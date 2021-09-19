0841cf4d88295cc55b2481a4f634cda1b6b0e658
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-19 20:47:34 +0000

    modified relationship betweeen tables for the correct table(Favorites), modified Table Planet in diameter propertie due to extra parentheses

--- src/models.py
+++ src/models.py
@@ -26,6 +26,7 @@ class Favorites(Base):
     UserID = Column(Integer, ForeignKey('user.id'))
     category = Column(String(30))
     favorite_name = Column(String(30))
+    users_id= Column(Integer, ForeignKey('users.id'))
     users = relationship(Users)
 
 class Vehicles(Base):
@@ -38,8 +39,8 @@ class Vehicles(Base):
     manufacturer = Column(String(30))
     passengers= Column(String(30))
     vehicle_class= Column(String(250))
-    user_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship(Users)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
+    favorites = relationship(Users)
 
 class Character(Base):
     __tablename__ = 'character'
@@ -50,8 +51,8 @@ class Character(Base):
     height = Column(Integer)
     birth_year = Column(String(30))
     gender = Column(String(30))
-    user_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship(Users)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
+    favorites = relationship(Users)
 
 class Planet(Base):
     __tablename__ = 'planet'
@@ -59,12 +60,12 @@ class Planet(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(30))
-    diameter = Column(Integer))
+    diameter = Column(Integer)
     climate = Column(String(30))
     terrain = Column(String(30))
     population = Column(String(30))
-    user_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship(Users)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
+    favorites = relationship(Favorites)
 
 
     def to_dict(self):
